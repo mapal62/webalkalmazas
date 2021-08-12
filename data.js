667d617bc4cef0338328c008ab0f1f372cc50224
mapal62 <mapal62@gmail.com>
2021-08-12 20:35:24 +0200

    arrow and data file

---
+++ data.js
@@ -0,0 +1,45 @@
+let kepParis = {
+    photo: "images/paris.jpg",
+    title: "Párizs (franciául: Paris)",
+    description: "Franciaország fővárosa. Az ország északi részén, a Szajna folyó partján terül el, az Île-de-France régió (vagy Région Parisienne) szívében. Párizs Európa egyik legnépesebb városa.",
+    vertical: "flexcenter"
+}
+
+let kepLondon = {
+    photo: "images/london.jpg",
+    title: "London az Egyesült Királyság fővárosa",
+    description: "a legnagyobb városi terület az Egyesült Királyságban; Európában Isztambul és Moszkva után a legnépesebb város. A Londoni-medencében, a Temze folyó két partján terül el. A története egészen az alapító rómaiakig nyúlik vissza, akik a várost Londiniumnak nevezték.",
+    vertical: "flexend"
+}
+let kepRoma = {
+    photo: "images/roma.jpg",
+    title: "Róma Olaszország fővárosa",
+    description: "Lazio régió központja (közigazgatásilag comune), a hajdani Római Birodalom központja. Az ország legnagyobb és legnépesebb városa 1285,3 km²-en mintegy 2,6 millió lakossal.[2] Területébe ékelve található a Vatikán, a római katolikus egyház központja.",
+    vertical: "flexcenter"
+}
+let kepBerlin = {
+    photo: "images/berlin.jpg",
+    title: "Berlin Németország fővárosa",
+    description: "tartományi rangú városállama (szövetségi tartománya) és a Berlin/Brandenburg nagyvárosi régió centruma. 3,6 millió lakosával Németország legnépesebb és kiterjedésében is legnagyobb városa, egyben az Európai Unió legnépesebb és legnagyobb városa.",
+    vertical: "flexcenter"
+}
+let kepMadrid = {
+    photo: "images/madrid.jpg",
+    title: "Madrid Spanyolország fővárosa",
+    description: "az azonos nevű tartomány székhelye. Területe: 607 km². Lakóinak száma 2011-ben 3,26 millió, elővárosokkal együtt kb. 6,5 millió fő. Berlin után a második legnépesebb város az Európai Unió területén.[2] Területének több mint 50%-át utcák, terek és parkok alkotják.",
+    vertical: "flexend"
+}
+let kepAthen = {
+    photo: "images/athen.jpg",
+    title: "Athén (görög: Αθήνα, Athína)",
+    description: "Görögország fővárosa. A modern Athén hatalmas, kozmopolita jellegű város; az ókori Athén nagy hatalmú városállam és jelentős tudományos központ volt. Lakossága elővárosokkal együtt körülbelül 3,5 millió, ez Görögország lakosságának mintegy 35%-a.",
+    vertical: "flexcenter"
+}
+let varosKepek = [
+    kepParis,
+    kepLondon,
+    kepRoma,
+    kepBerlin,
+    kepMadrid,
+    kepAthen
+];
--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-let kepParis = {
-    photo: "images/paris.jpg",
-    title: "Párizs (franciául: Paris)",
-    description: "Franciaország fővárosa. Az ország északi részén, a Szajna folyó partján terül el, az Île-de-France régió (vagy Région Parisienne) szívében. Párizs Európa egyik legnépesebb városa."
-}
-
-let kepLondon = {
-    photo: "images/london.jpg",
-    title: "London az Egyesült Királyság és azon belül Anglia fővárosa",
-    description: "a legnagyobb városi terület az Egyesült Királyságban; Európában Isztambul és Moszkva után a legnépesebb város. A Londoni-medencében, a Temze folyó két partján terül el. A története egészen az alapító rómaiakig nyúlik vissza, akik a várost Londiniumnak nevezték. A középkor emlékeit a város központjában található City of London őrzi, a mai London e köré épült ki."
-}
-let kepRoma = {
-    photo: "images/roma.jpg",
-    title: "Róma Olaszország fővárosa",
-    description: "Lazio régió központja (közigazgatásilag comune), a hajdani Római Birodalom központja. Az ország legnagyobb és legnépesebb városa 1285,3 km²-en mintegy 2,6 millió lakossal.[2] Területébe ékelve található a Vatikán, a római katolikus egyház központja, a pápa székhelye, a világ legkisebb független állama."
-}
-
 let melyikVaros = 0;
-let varosKepek = [kepParis, kepLondon, kepRoma];
 
 function valasztas(varos) {
     $('#photo').attr('src', varos.photo);
     $('#photo-title').text(varos.title);
     $('#photo-description').text(varos.description);
+    if (varos.vertical === "flexend") {
+        $('.kozepkep').removeClass("flexstart")
+        $('.kozepkep').removeClass("flexcenter")
+        $('.kozepkep').addClass(varos.vertical)
+    }
+    if (varos.vertical === "flexstart") {
+        $('.kozepkep').removeClass("flexstart")
+        $('.kozepkep').removeClass("flexcend")
+        $('.kozepkep').addClass(varos.vertical)
+    }
+    if (varos.vertical === "flexcenter") {
+        $('.kozepkep').removeClass("flexend")
+        $('.kozepkep').removeClass("flexcenter")
+        $('.kozepkep').addClass(varos.vertical)
+    }
 }
-$('#kovetkezo').click(() => {
-    console.log(melyikVaros);
+$('.jobbra').click(elore);
+$('.balra').click(vissza);
+$(document).keydown((event) => {
+    if (event.which === 39) {
+        elore();
+    }
+})
+$(document).keydown((event) => {
+    if (event.which === 37) {
+        vissza();
+    }
+})
+
+function elore() {
     melyikVaros = melyikVaros + 1;
-    console.log(melyikVaros);
     if (melyikVaros >= varosKepek.length) {
         melyikVaros = 0;
     }
     valasztas(varosKepek[melyikVaros]);
-})
+}
+
+function vissza() {
+    melyikVaros = melyikVaros - 1;
+    if (melyikVaros < 0) {
+        melyikVaros = varosKepek.length - 1;
+    }
+    valasztas(varosKepek[melyikVaros]);
+}
