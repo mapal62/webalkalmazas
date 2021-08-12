35786efbc29a794599dfaac323a92e4cdb249409
mapal62 <mapal62@gmail.com>
2021-08-12 16:28:03 +0200

    call the city

---
+++ script.js
@@ -0,0 +1,22 @@
+let kepParis = {
+    photo: "images/paris.jpg",
+    title: "Párizs (franciául: Paris)",
+    description: "Franciaország fővárosa. Az ország északi részén, a Szajna folyó partján terül el, az Île-de-France régió (vagy Région Parisienne) szívében. Párizs Európa egyik legnépesebb városa."
+}
+
+let kepLondon = {
+    photo: "images/london.jpg",
+    title: "London az Egyesült Királyság és azon belül Anglia fővárosa",
+    description: "a legnagyobb városi terület az Egyesült Királyságban; Európában Isztambul és Moszkva után a legnépesebb város. A Londoni-medencében, a Temze folyó két partján terül el. A története egészen az alapító rómaiakig nyúlik vissza, akik a várost Londiniumnak nevezték. A középkor emlékeit a város központjában található City of London őrzi, a mai London e köré épült ki."
+}
+let kepRoma = {
+    photo: "images/roma.jpg",
+    title: "Róma Olaszország fővárosa",
+    description: "Lazio régió központja (közigazgatásilag comune), a hajdani Római Birodalom központja. Az ország legnagyobb és legnépesebb városa 1285,3 km²-en mintegy 2,6 millió lakossal.[2] Területébe ékelve található a Vatikán, a római katolikus egyház központja, a pápa székhelye, a világ legkisebb független állama."
+}
+
+function valasztas(varos) {
+    $('#photo').attr('src', varos.photo);
+    $('#photo-title').text(varos.title);
+    $('#photo-description').text(varos.description);
+}
