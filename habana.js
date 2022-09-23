ef9976c9efb1cf8716f14a6bd0ad72aec25225fe
mapal62 <mapal62@gmail.com>
2022-09-23 21:07:33 +0200

    Make fun for Olga Lidia -use data in file habana.js -get images and description from wikipedia

---
+++ habana.js
@@ -0,0 +1,52 @@
+let kepBahia = {
+    photo: "images/bahia.jpg",
+    title: "Bahía de La Habana",
+    description: "En la bahía se ubica el puerto de La Habana, calificado históricamente como uno de los más abrigados y seguros de América.",
+    vertical: "flexend"
+}
+
+let kepClima = {
+    photo: "images/clima.jpg",
+    title: "Clima",
+    description: "La Habana, como la mayor parte de Cuba posee un clima tropical que es templado debido a la posición de la isla dentro del cinturón de los vientos alisios y por las cálidas corrientes marinas.",
+    vertical: "flexend"
+}
+let kepTemp = {
+    photo: "images/temperature.jpg",
+    title: "Parámetros climáticos",
+    description: "Las precipitaciones son abundantes en octubre y septiembre y bastante escasas entre marzo y mayo.",
+    vertical: "flexcenter"
+}
+let kepColonial = {
+    photo: "images/cathedral.jfif",
+    title: "Colonial española y barroca",
+    description: "Durante la colonia se trajeron las grandes riquezas de la América Española que conectaban a través de La Habana como un punto de concentración para el trasbordo importante entre el Nuevo Mundo y Europa.",
+    vertical: "flexscenter"
+}
+let kepCentro = {
+    photo: "images/centrocultural.jpg",
+    title: "Arte Mudéjar",
+    description: "Esta arquitectura se había engendrado debido al clima cálido del terreno, que buscaba interiores frescos y amplios, a la gran oferta de maderas finas que se requerían para estas construcciones.",
+    vertical: "flexend"
+}
+let kepPlaza = {
+    photo: "images/sanfrancisco.jpg",
+    title: "Plaza San Francisco de Asís:",
+    description: "La plaza suele utilizarse para realizar exposiciones artísticas, como la exposición de los United Buddy Bears tuvo lugar en 2015 y que constaba de 128 esculturas de dos metros de alto, cada una pintada por un artista de uno de los 128 países.",
+    vertical: "flexend"
+}
+let kepHavana = {
+    photo: "images/havana.jpg",
+    title: "Mapa alemán de La Habana, en 1888.",
+    description: "La Habana resurgió en varias ocasiones de los escombros y cenizas a que la reducían de cuando en cuando los piratas y corsarios franceses durante la primera mitad del siglo xvi.",
+    vertical: "flexcenter"
+}
+let varosKepek = [
+    kepBahia,
+    kepClima,
+    kepTemp,
+    kepColonial,
+    kepCentro,
+    kepPlaza,
+    kepHavana
+];
